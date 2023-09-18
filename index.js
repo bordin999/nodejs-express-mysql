d35e47fb92f9c42f6643e2c50c80dd8da5194b23
bordin999 <bordin999@gmail.com>
2023-09-18 17:09:12 +0700

    CRUD

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -28,9 +30,78 @@ app.get("/", (req, res) => {
         }
         res.json(result);
     })
-
 })
-    
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM users WHERE id = ${id}`;
+    con.query(sql, (err, result) => {
+        if(err) {
+            console.log(err);
+            res.status(500).json({
+                status: "error",
+                message: err.message
+            })
+            return;
+        }
+        res.json(result);
+    })
+})
+
+app.post("/add", (req, res) => {
+    const users = req.body; 
+    const values = users.map(user => [user.name, user.email, user.phone]);
+    const sql = "INSERT INTO users (name, email, phone) VALUES ?";
+
+    con.query(sql, [values], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({
+                status: "error",
+                message: err.message
+            });
+            return;
+        }
+        res.json(result);
+    });
+});
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM users WHERE id = ${id}`;
+    con.query(sql, (err, result) => {
+        if(err) {
+            console.log(err);
+            res.status(500).json({
+                status: "error",
+                message: err.message
+            })
+            return;
+        }
+        res.json(result);
+    })
+})
+
+app.post("/update", (req, res) => {
+    const users = req.body; 
+    const sql = "UPDATE users SET name = ?, email = ?, phone = ? WHERE id = ?";
+
+    users.forEach(user => {
+        con.query(sql, [user.name, user.email, user.phone, user.id], (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({
+                    status: "error",
+                    message: err.message
+                });
+                return;
+            }
+            console.log(`Updated user with ID ${user.id}`);
+        });
+    });
+
+    res.json({ status: "success", message: "Users updated successfully" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
